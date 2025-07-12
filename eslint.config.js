a249778a7eaae186b8a2790d9fe0e9fdae048776
Braian-Quintian <bquintianromero@gmail.com>
2025-07-12 18:08:19 -0500

    feat: :sparkles: Nueva implementación

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// eslint.config.js
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      // Buenas prácticas generales
+      'no-console': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'off', // innecesario con TypeScript
+
+      // TypeScript específicas
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+
+      // Reglas opcionales según preferencia
+      '@typescript-eslint/consistent-type-imports': 'warn',
+    },
+  },
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
